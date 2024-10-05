a391d96056fbbff3f447f5a1fb180d530481d703
CayohAllou <cayohallouu@gmail.com>
2024-10-05 16:43:08 +0800

    Initial commit

---
+++ src/components/gallery.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="blog">
+    <h1>gallery</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gallery',
+  data() {
+    return {
+      title: 'Gallery',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
